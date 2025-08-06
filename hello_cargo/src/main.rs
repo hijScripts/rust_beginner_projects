91a54c4ec6d3c19689cd9545dfede21f45746bab
Harrison J <harrisonjenkins1902@gmail.com>
2025-08-06 16:48:07 +0930

    Added notes

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
     println!("Hello, world!");
 }
+
+// compile: cargo build
+// execute: ./target/debug/[cargo dir name]
+
+// compile + run: cargo run
+
+// compile w/out executable: cargo check
+
+// build for release: cargo build --release (goes to target/release)
