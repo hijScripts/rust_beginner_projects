9c5fbaebabf1606aeb7034ff495122bf56d0ed9c
Harrison J <harrisonjenkins1902@gmail.com>
2025-08-06 17:21:40 +0930

    Added documentation + prints secret num

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,10 +1,15 @@
 use std::io; // input/output library, comes from standard library
-use rand::Rng; // random num gen, comes from rand library
+use rand::Rng; // Rng trait, comes from rand library
+
+// open local doc with all info for dependencies used: cargo doc --open
 
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let secret_number = rand::thread_rng().gen_range(1..=100); // creates number local to current thread of execution and seeded by the OS.
+                                                               // then generates a range from 1-100 inclusive of the lower and upper.
+
+    println!("The secret number is: {secret_number}");
 
     println!("Please input your guess.");
 
