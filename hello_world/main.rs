41672b541e31afedf1c2ab610937e80569e14db7
Harrison J <harrisonjenkins1902@gmail.com>
2025-08-06 16:33:02 +0930

    Added notes

--- hello_world/main.rs
+++ hello_world/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world!"); // ! indicates a macro is being used rather than function.
 }
+
+// compile: rustc [filepath]
+// execute: ./[filename]
+// ls: displays all files in directory
