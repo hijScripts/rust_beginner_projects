6726388110dfca3accca26376166ba5b76074a02
Harrison J <harrisonjenkins1902@gmail.com>
2025-08-09 14:05:08 +0930

    int types and their ranges

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -1,7 +1,30 @@
-fn main() {
+// Example of delcaring data type
+// fn main() {
 
-    // Without the ': u32', compile-time error will generate
-    let guess: u32 = "42".parse().expect("Not a number!");
+//     // Without the ': u32', compile-time error will generate
+//     let guess: u32 = "42".parse().expect("Not a number!");
 
-    println!("{guess}")
-}
+//     println!("{guess}")
+// }
+
+/* Scalar Data Types 
+Integers
+Floating-point numbers
+Booleans
+Characters
+*/
+
+/* Types of Integers (Signed: i (for negatives & positives), 
+                      Unsigned: u (for only positives))
+Length
+8-bit	i8	u8
+16-bit	i16	u16
+32-bit	i32	u32
+64-bit	i64	u64
+128-bit	i128	u128
+architecture dependent	isize	usize
+
+inclusive ranges, n = amt of bits
+unsigned range: 0 to 2n − 1
+signed range: −(2^(n − 1)) to 2^(n − 1) − 1
+*/
