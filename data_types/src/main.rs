80c235e72c5a979bd2e0ff30c0f1484b60280c42
Harrison J <harrisonjenkins1902@gmail.com>
2025-08-09 14:14:10 +0930

    Tuples

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -36,10 +36,75 @@ Length
 
 f64 considered as fast as f32 nowadays with more precision.
 */
+// fn main() {
+//     let x = 2.0; // f64
+
+//     let y: f32 = 3.0; // f32
+
+//     println!("{x}, {y}")
+// }
+
+/* Numeric Operations */
+// fn main() {
+//     // addition
+//     let sum = 5 + 10;
+
+//     // subtraction
+//     let difference = 95.5 - 4.3;
+
+//     // multiplication
+//     let product = 4 * 30;
+
+//     // division
+//     let quotient = 56.7 / 32.2;
+//     let truncated = -5 / 3; // Results in -1
+
+//     // remainder
+//     let remainder = 43 % 5;
+// }
+
+/* Boolean Type (1 byte in size) */
+// fn main() {
+//     let t = true;
+
+//     let f: bool = false; // with explicit type annotation
+// }
+
+/* Char Type */
 fn main() {
-    let x = 2.0; // f64
-
-    let y: f32 = 3.0; // f32
-
-    println!("{x}, {y}")
+    let c = 'z';
+    let z: char = 'ℤ'; // with explicit type annotation
+    let heart_eyed_cat = '😻';
 }
+
+/* Compound Types 
+Tuples
+Array
+*/
+
+/* Tuples */
+fn main() {
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+}
+
+// Getting individual value out of a tuple with a pattern
+fn main() {
+    let tup = (500, 6.4, 1);
+
+    let (x, y, z) = tup;
+
+    println!("The value of y is: {y}");
+}
+
+// Getting individual value out of a tuple with indexing
+fn main() {
+    let x: (i32, f64, u8) = (500, 6.4, 1);
+
+    let five_hundred = x.0;
+
+    let six_point_four = x.1;
+
+    let one = x.2;
+}
+
+/* Array */
