b97c7b248fff036d8d7fbc9d328b62cd34b5faaf
Harrison J <harrisonjenkins1902@gmail.com>
2025-08-09 14:21:08 +0930

    arrays

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -107,4 +107,48 @@ fn main() {
     let one = x.2;
 }
 
-/* Array */
+/* Arrays */
+fn main() {
+    let a = [1, 2, 3, 4, 5];
+
+    let months = ["January", "February", "March", "April", "May", "June", "July",
+              "August", "September", "October", "November", "December"];
+
+    // [data type, num of elements]
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+
+    // defines an array with 5 elements of value 3
+    let a = [3; 5];
+}
+
+// accessing an array with indexing
+fn main() {
+    let a = [1, 2, 3, 4, 5];
+
+    let first = a[0];
+    let second = a[1];
+}
+
+// compiles but will result in a run-time error if out of bounds.
+use std::io;
+
+fn main() {
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!("The value of the element at index {index} is: {element}");
+}
