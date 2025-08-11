fb3d1a96ddd4f6d3c65453a47be690b57fb07d88
Harrison J <harrisonjenkins1902@gmail.com>
2025-08-12 00:06:29 +0930

    implicit returning and parameters

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,9 +1,41 @@
-fn main() {
-    println!("Hello, world!");
+// fn main() {
+//     println!("Hello, world!");
 
-    another_function();
+//     another_function(5, 'h');
+// }
+
+// parameters speical vars the are apart of functions signature. 
+// Interchangable with arguments that are concrete values
+fn another_function(x: i32, unit_label: char) {
+    println!("Another function. {x}, {unit_label}");
 }
 
-fn another_function() {
-    println!("Another function.");
+// Statements are instructions that perform some action and do not return a value.
+// Expressions evaluate to a resultant value.
+
+//statement
+// fn main() {
+//     let x = 5;
+// }
+
+//expression. Expressions don't end with ; as it turns it into a statement
+// prevents a value from being returned
+// fn main() {
+//     let y = {
+//         let x = 3;
+//         x + 1
+//     };
+
+//     println!("The value of y is: {y}");
+// }
+
+// implicit returning in functions.
+fn five() -> i32 {
+    5
+}
+
+fn main() {
+    let x = five();
+
+    println!("The value of x is: {x}");
 }
