6d86a43746643189d20c99ba662ec23007cae1f3
Harrison J <harrisonjenkins1902@gmail.com>
2025-08-12 00:24:59 +0930

    loops !!

--- control_flow/src/main.rs
+++ control_flow/src/main.rs
@@ -1,3 +1,132 @@
+// fn main() {
+//     let number = 3;
+
+//     if number < 5 {
+//         println!("condition was true");
+//     } else {
+//         println!("condition was false");
+//     }
+// }
+
+// this does not run as rust does not convert non-bool to bool
+// fn main() {
+//     let number = 1;
+
+//     if number {
+//         println!("number was three");
+//     }
+// }
+
+// else if example
+// fn main() {
+//     let number = 6;
+
+//     if number % 4 == 0 {
+//         println!("number is divisible by 4");
+//     } else if number % 3 == 0 {
+//         println!("number is divisible by 3");
+//     } else if number % 2 == 0 {
+//         println!("number is divisible by 2");
+//     } else {
+//         println!("number is not divisible by 4, 3, or 2");
+//     }
+// }
+
+// using if statements to assign values. all values need to be same type
 fn main() {
-    println!("Hello, world!");
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+
+    println!("The value of number is: {number}");
 }
+
+// 'loop' loop
+fn main() {
+    loop {
+        println!("again!");
+    }
+}
+
+// using 'loop' to get value
+fn main() {
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {result}");
+}
+
+// loop labels
+// fn main() {
+//     let mut count = 0;
+//     'counting_up: loop {
+//         println!("count = {count}");
+//         let mut remaining = 10;
+
+//         loop {
+//             println!("remaining = {remaining}");
+//             if remaining == 9 {
+//                 break;
+//             }
+//             if count == 2 {
+//                 break 'counting_up;
+//             }
+//             remaining -= 1;
+//         }
+
+//         count += 1;
+//     }
+//     println!("End count = {count}");
+// }
+
+// 'while' loop
+// fn main() {
+//     let mut number = 3;
+
+//     while number != 0 {
+//         println!("{number}!");
+
+//         number -= 1;
+//     }
+
+//     println!("LIFTOFF!!!");
+// }
+
+// while loop for an array
+// fn main() {
+//     let a = [10, 20, 30, 40, 50];
+//     let mut index = 0;
+
+//     while index < 5 {
+//         println!("the value is: {}", a[index]);
+
+//         index += 1;
+//     }
+// }
+
+// 'for' loop for an array. more efficient and less error prone
+// fn main() {
+//     let a = [10, 20, 30, 40, 50];
+
+//     for element in a {
+//         println!("the value is: {element}");
+//     }
+// }
+
+// 'for' loop as a countdown
+fn main() {
+    for number in (1..4).rev() {
+        println!("{number}!");
+    }
+    println!("LIFTOFF!!!");
+}
+
+//Convert temperatures between Fahrenheit and Celsius.
+//Generate the nth Fibonacci number.
+//Print the lyrics to the Christmas carol “The Twelve Days of Christmas,” taking advantage of the repetition in the song.
