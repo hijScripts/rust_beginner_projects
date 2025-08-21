f3f9333a84a233ab555f4998dd16b6bb943f3e03
Harrison J <harrisonjenkins1902@gmail.com>
2025-08-21 23:48:06 +0930

    Example of scope ownership

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,3 +1,14 @@
+// Ownership Rules
+// First, let’s take a look at the ownership rules. Keep these rules in mind as we work through the examples that illustrate them:
+
+// Each value in Rust has an owner.
+// There can only be one owner at a time.
+// When the owner goes out of scope, the value will be dropped.
+
 fn main() {
-    println!("Hello, world!");
+       {                      // s is not valid here, since it's not yet declared
+        let s = "hello";   // s is valid from this point forward
+
+        // do stuff with s
+    }   
 }
