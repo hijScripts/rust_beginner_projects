36294b5344b1235c84879551d568959a45bfa71b
Harrison J <harrisonjenkins1902@gmail.com>
2025-08-23 00:55:53 +0930

    mutable references

--- references/src/main.rs
+++ references/src/main.rs
@@ -9,4 +9,28 @@ fn main() {
 
 fn calculate_length(s: &String) -> usize {
     s.len()
+}   // Here, s goes out of scope. But because s does not have ownership of what
+    // it refers to, the String is not dropped.
+
+// ** mutable references ** //
+fn main() {
+    let mut s = String::from("hello");
+
+    change(&mut s);
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
+
+
+// this wont run as it violates the borrowing rules. Can only have one mutable 
+// reference to a piece of data in a particular scope.
+fn main() {
+    let mut s = String::from("hello");
+
+    let r1 = &mut s;
+    let r2 = &mut s;
+
+    println!("{r1}, {r2}");
 }
