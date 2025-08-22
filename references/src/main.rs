be97d5a8bb364e994b423fe6c75399494439a810
Harrison J <harrisonjenkins1902@gmail.com>
2025-08-23 01:02:22 +0930

    danging references

--- references/src/main.rs
+++ references/src/main.rs
@@ -60,3 +60,23 @@ fn main() {
     let r3 = &mut s; // no problem
     println!("{r3}");
 }
+
+// dangling references
+fn main() {
+    let reference_to_nothing = dangle();
+}
+
+fn dangle() -> &String { // dangle returns a reference to a String
+
+    let s = String::from("hello"); // s is a new String
+
+    &s // we return a reference to the String, s
+} // Here, s goes out of scope and is dropped, so its memory goes away.
+  // Danger!
+
+// The Rules of References
+// Let’s recap what we’ve discussed about references:
+
+// At any given time, you can have either one mutable reference or any number of immutable references.
+// References must always be valid.
+// Next, we’ll look at a different kind of reference: slices.
