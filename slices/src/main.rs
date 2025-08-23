01315db4064cd279b1e96c24526eba55eaff92d2
Harrison J <harrisonjenkins1902@gmail.com>
2025-08-23 13:12:46 +0930

    examples with and without slices.

--- slices/src/main.rs
+++ slices/src/main.rs
@@ -1,3 +1,32 @@
+// getting first word of a string without slices
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
+}
+
+// problem with above
 fn main() {
-    println!("Hello, world!");
+    let mut s = String::from("hello world");
+
+    let word = first_word(&s); // word will get the value 5
+
+    s.clear(); // this empties the String, making it equal to ""
+
+    // word still has the value 5 here, but s no longer has any content that we
+    // could meaningfully use with the value 5, so word is now totally invalid!
+}
+
+// slices
+fn main() {
+    let s = String::from("hello world");
+
+    let hello = &s[0..5];
+    let world = &s[6..11];
 }
