7b3ad1b4d36a2bae68211d29cabe96cb9305b5e7
Harrison J <harrisonjenkins1902@gmail.com>
2025-08-27 11:57:02 +0930

    added some debugging examples

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,13 +1,35 @@
+#[derive(Debug)] // allows us to print the struct for debugging
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
-    let width1 = 30;
-    let height1 = 50;
+    let rec1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(width1, height1)
+        area(&rec1)
     );
+
+    // :? - simple debug print
+    // :#? - pretty debug print
+    println!("rec1 is {rec1:#?}"); // debug print
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+fn main() {
+    let scale = 2;
+    let rect1 = Rectangle {
+        width: dbg!(30 * scale),
+        height: 50,
+    };
+
+    dbg!(&rect1);
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
