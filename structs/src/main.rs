91a7cce13f1f29ef03362958a1effb50e3a6bb43
Harrison J <harrisonjenkins1902@gmail.com>
2025-08-27 11:29:16 +0930

    added example build_user func

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -22,3 +22,12 @@ fn main() {
 
     user2.email = String::from("anotheremail@example.com");
 }
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username: username,
+        email: email,
+        sign_in_count: 1,
+    }
+}
