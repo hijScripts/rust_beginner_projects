0c96714889b1d1a8fac781f10a320a3a86af74df
Harrison J <harrisonjenkins1902@gmail.com>
2025-08-27 12:06:45 +0930

    added example of an associated func

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -49,6 +49,13 @@ impl Rectangle {
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
     }
+
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
 }
 
 // fn main() {
@@ -77,6 +84,9 @@ fn main() {
         height: 45,
     };
 
+    let square1 = Rectangle::square(3);
+
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+    println!("square1 is {}", square1.area());
 }
