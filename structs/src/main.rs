5e2495dce07a846ad3275cb2b91242b131fd7dd7
Harrison J <harrisonjenkins1902@gmail.com>
2025-08-27 11:27:38 +0930

    Added example struct

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,3 +1,15 @@
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let user1 = User {
+        active: true,
+        username: String::from("someusername123"),
+        email: String::from("someone@example.com"),
+        sign_in_count: 1,
+    };
 }
