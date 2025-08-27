a3234239679c749c0518fd34c900c15b67ab0c4d
Harrison J <harrisonjenkins1902@gmail.com>
2025-08-27 11:58:32 +0930

    added an area method specific to rectangle

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,35 +1,59 @@
-#[derive(Debug)] // allows us to print the struct for debugging
+// #[derive(Debug)] // allows us to print the struct for debugging
+// struct Rectangle {
+//     width: u32,
+//     height: u32,
+// }
+
+// fn main() {
+//     let rec1 = Rectangle {
+//         width: 30,
+//         height: 50,
+//     };
+
+//     println!(
+//         "The area of the rectangle is {} square pixels.",
+//         area(&rec1)
+//     );
+
+//     // :? - simple debug print
+//     // :#? - pretty debug print
+//     println!("rec1 is {rec1:#?}"); // debug print
+// }
+
+// fn main() {
+//     let scale = 2;
+//     let rect1 = Rectangle {
+//         width: dbg!(30 * scale),
+//         height: 50,
+//     };
+
+//     dbg!(&rect1);
+// }
+
+// fn area(rectangle: &Rectangle) -> u32 {
+//     rectangle.width * rectangle.height
+// }
+
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
 fn main() {
-    let rec1 = Rectangle {
+    let rect1 = Rectangle {
         width: 30,
         height: 50,
     };
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(&rec1)
+        rect1.area()
     );
-
-    // :? - simple debug print
-    // :#? - pretty debug print
-    println!("rec1 is {rec1:#?}"); // debug print
-}
-
-fn main() {
-    let scale = 2;
-    let rect1 = Rectangle {
-        width: dbg!(30 * scale),
-        height: 50,
-    };
-
-    dbg!(&rect1);
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
 }
