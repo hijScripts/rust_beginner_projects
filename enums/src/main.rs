36b2970cd770869c89374a8cd69bfcf1a5ab741d
Harrison J <harrisonjenkins1902@gmail.com>
2025-08-28 12:59:51 +0930

    finished chapter 6

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -151,3 +151,29 @@ fn test(num: u8) {
 fn main() {
     test(1);
 }
+
+// example using match
+let config_max = Some(3u8);
+match config_max {
+    Some(max) => println!("The maximum is configured to be {max}"),
+    _ => (),
+}
+
+// example using if let
+let config_max = Some(3u8);
+if let Some(max) = config_max {
+    println!("The maximum is configured to be {max}");
+}
+
+// let else example
+fn describe_state_quarter(coin: Coin) -> Option<String> {
+    let Coin::Quarter(state) = coin else {
+        return None;
+    };
+
+    if state.existed_in(1900) {
+        Some(format!("{state:?} is pretty old, for America!"))
+    } else {
+        Some(format!("{state:?} is relatively new."))
+    }
+}
