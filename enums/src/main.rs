4cc99aa56a17bc8c5834437f7bd3dfe74aefd88b
Harrison J <harrisonjenkins1902@gmail.com>
2025-08-28 12:51:31 +0930

    added heaps more examples

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -93,3 +93,49 @@ fn main() {
 
     println!("Value: {value}");
 }
+
+// - Matching with Option<T> - //
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+let five = Some(5);
+let six = plus_one(five);
+let none = plus_one(None);
+
+// - Catch All with 'other' and '_' - //
+let dice_roll = 9;
+match dice_roll {
+    3 => add_fancy_hat(),
+    7 => remove_fancy_hat(),
+    other => move_player(other),
+}
+
+fn add_fancy_hat() {}
+fn remove_fancy_hat() {}
+fn move_player(num_spaces: u8) {}
+
+
+let dice_roll = 9;
+match dice_roll {
+    3 => add_fancy_hat(),
+    7 => remove_fancy_hat(),
+    _ => reroll(), // no longer binds to a variable
+}
+
+fn add_fancy_hat() {}
+fn remove_fancy_hat() {}
+fn reroll() {}
+
+let dice_roll = 9;
+match dice_roll {
+    3 => add_fancy_hat(),
+    7 => remove_fancy_hat(),
+    _ => (), // nothing happens, unit value (empty tuple)
+}
+
+fn add_fancy_hat() {}
+fn remove_fancy_hat() {}
