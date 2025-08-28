2f7b4502769fe4b5afe8525bad4d3fcc489ef142
Harrison J <harrisonjenkins1902@gmail.com>
2025-08-28 12:26:07 +0930

    ption<T> Enum, Some, None

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -38,3 +38,9 @@ enum IpAddr {
 let home = IpAddr::V4(127, 0, 0, 1);
 
 let loopback = IpAddr::V6(String::from("::1"));
+
+// - Option<T> Enum, Some, None - //
+let some_number = Some(5);
+let some_char = Some('e');
+
+let absent_number: Option<i32> = None;
