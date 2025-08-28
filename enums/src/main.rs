acca94eb01d2932b5199284a50295e5560641883
Harrison J <harrisonjenkins1902@gmail.com>
2025-08-28 12:53:32 +0930

    testing stuff

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,56 +1,79 @@
-// // - Beginner Way - //
-// enum IpAddrKind {
-//     V4,
-//     V6,
+// // // - Beginner Way - //
+// // enum IpAddrKind {
+// //     V4,
+// //     V6,
+// // }
+
+// // struct IpAddr {
+// //     kind: IpAddrKind,
+// //     address: String,
+// // }
+
+// // let home = IpAddr {
+// //     kind: IpAddrKind::V4,
+// //     address: String::from("127.0.0.1"),
+// // };
+
+// // let loopback = IpAddr {
+// //     kind: IpAddrKind::V6,
+// //     address: String::from("::1"),
+// // };
+
+// // // - 'Advanced' Way - //
+// // enum IpAddr {
+// //     V4(String),
+// //     V6(String),
+// // }
+
+// // let home = IpAddr::V4(String::from("127.0.0.1"));
+
+// // let loopback = IpAddr::V6(String::from("::1"));
+
+// // // - Different Data Types - //
+// // enum IpAddr {
+// //     V4(u8, u8, u8, u8),
+// //     V6(String),
+// // }
+
+// // let home = IpAddr::V4(127, 0, 0, 1);
+
+// // let loopback = IpAddr::V6(String::from("::1"));
+
+// // // - Option<T> Enum, Some, None - //
+// // let some_number = Some(5);
+// // let some_char = Some('e');
+
+// // let absent_number: Option<i32> = None;
+
+// // // - Match Control Flow Operator - //
+// // enum Coin {
+// //     Penny,
+// //     Nickel,
+// //     Dime,
+// //     Quarter,
+// // }
+
+// // fn value_in_cents(coin: Coin) -> u8 {
+// //     match coin {
+// //         Coin::Penny => 1,
+// //         Coin::Nickel => 5,
+// //         Coin::Dime => 10,
+// //         Coin::Quarter => 25,
+// //     }
+// // }
+
+// #[derive(Debug)] // so we can inspect the state in a minute
+// enum UsState {
+//     Alabama,
+//     Alaska,
+//     // --snip--
 // }
 
-// struct IpAddr {
-//     kind: IpAddrKind,
-//     address: String,
-// }
-
-// let home = IpAddr {
-//     kind: IpAddrKind::V4,
-//     address: String::from("127.0.0.1"),
-// };
-
-// let loopback = IpAddr {
-//     kind: IpAddrKind::V6,
-//     address: String::from("::1"),
-// };
-
-// // - 'Advanced' Way - //
-// enum IpAddr {
-//     V4(String),
-//     V6(String),
-// }
-
-// let home = IpAddr::V4(String::from("127.0.0.1"));
-
-// let loopback = IpAddr::V6(String::from("::1"));
-
-// // - Different Data Types - //
-// enum IpAddr {
-//     V4(u8, u8, u8, u8),
-//     V6(String),
-// }
-
-// let home = IpAddr::V4(127, 0, 0, 1);
-
-// let loopback = IpAddr::V6(String::from("::1"));
-
-// // - Option<T> Enum, Some, None - //
-// let some_number = Some(5);
-// let some_char = Some('e');
-
-// let absent_number: Option<i32> = None;
-
-// // - Match Control Flow Operator - //
 // enum Coin {
 //     Penny,
 //     Nickel,
 //     Dime,
-//     Quarter,
+//     Quarter(UsState),
 // }
 
 // fn value_in_cents(coin: Coin) -> u8 {
@@ -58,84 +81,73 @@
 //         Coin::Penny => 1,
 //         Coin::Nickel => 5,
 //         Coin::Dime => 10,
-//         Coin::Quarter => 25,
+//         Coin::Quarter(state) => {
+//             println!("State quarter from {state:?}!");
+//             25
+//         }
 //     }
 // }
 
-#[derive(Debug)] // so we can inspect the state in a minute
-enum UsState {
-    Alabama,
-    Alaska,
-    // --snip--
-}
+// fn main() {
+//     let value: u8 = value_in_cents(Coin::Quarter(UsState::Alaska));
 
-enum Coin {
-    Penny,
-    Nickel,
-    Dime,
-    Quarter(UsState),
-}
+//     println!("Value: {value}");
+// }
 
-fn value_in_cents(coin: Coin) -> u8 {
-    match coin {
-        Coin::Penny => 1,
-        Coin::Nickel => 5,
-        Coin::Dime => 10,
-        Coin::Quarter(state) => {
-            println!("State quarter from {state:?}!");
-            25
-        }
+// // - Matching with Option<T> - //
+// fn plus_one(x: Option<i32>) -> Option<i32> {
+//     match x {
+//         None => None,
+//         Some(i) => Some(i + 1),
+//     }
+// }
+
+// let five = Some(5);
+// let six = plus_one(five);
+// let none = plus_one(None);
+
+// // - Catch All with 'other' and '_' - //
+// let dice_roll = 9;
+// match dice_roll {
+//     3 => add_fancy_hat(),
+//     7 => remove_fancy_hat(),
+//     other => move_player(other),
+// }
+
+// fn add_fancy_hat() {}
+// fn remove_fancy_hat() {}
+// fn move_player(num_spaces: u8) {}
+
+
+// let dice_roll = 9;
+// match dice_roll {
+//     3 => add_fancy_hat(),
+//     7 => remove_fancy_hat(),
+//     _ => reroll(), // no longer binds to a variable and rerolls
+// }
+
+// fn add_fancy_hat() {}
+// fn remove_fancy_hat() {}
+// fn reroll() {}
+
+// let dice_roll = 9;
+// match dice_roll {
+//     3 => add_fancy_hat(),
+//     7 => remove_fancy_hat(),
+//     _ => (), // nothing happens, unit value (empty tuple)
+// }
+
+// fn add_fancy_hat() {}
+// fn remove_fancy_hat() {}
+
+fn test(num: u8) {
+    match num {
+        1 => println!("one"),
+        2 => println!("two"),
+        _ => (),
     }
 }
 
 fn main() {
-    let value: u8 = value_in_cents(Coin::Quarter(UsState::Alaska));
-
-    println!("Value: {value}");
+    test(1);
 }
-
-// - Matching with Option<T> - //
-fn plus_one(x: Option<i32>) -> Option<i32> {
-    match x {
-        None => None,
-        Some(i) => Some(i + 1),
-    }
-}
-
-let five = Some(5);
-let six = plus_one(five);
-let none = plus_one(None);
-
-// - Catch All with 'other' and '_' - //
-let dice_roll = 9;
-match dice_roll {
-    3 => add_fancy_hat(),
-    7 => remove_fancy_hat(),
-    other => move_player(other),
-}
-
-fn add_fancy_hat() {}
-fn remove_fancy_hat() {}
-fn move_player(num_spaces: u8) {}
-
-
-let dice_roll = 9;
-match dice_roll {
-    3 => add_fancy_hat(),
-    7 => remove_fancy_hat(),
-    _ => reroll(), // no longer binds to a variable
-}
-
-fn add_fancy_hat() {}
-fn remove_fancy_hat() {}
-fn reroll() {}
-
-let dice_roll = 9;
-match dice_roll {
-    3 => add_fancy_hat(),
-    7 => remove_fancy_hat(),
-    _ => (), // nothing happens, unit value (empty tuple)
-}
-
-fn add_fancy_hat() {}
-fn remove_fancy_hat() {}
