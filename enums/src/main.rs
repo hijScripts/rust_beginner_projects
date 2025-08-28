502b75014c01a161e82407200d83a2d28c4ed973
Harrison J <harrisonjenkins1902@gmail.com>
2025-08-28 12:40:22 +0930

    example code added

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,46 +1,95 @@
-// - Beginner Way - //
-enum IpAddrKind {
-    V4,
-    V6,
+// // - Beginner Way - //
+// enum IpAddrKind {
+//     V4,
+//     V6,
+// }
+
+// struct IpAddr {
+//     kind: IpAddrKind,
+//     address: String,
+// }
+
+// let home = IpAddr {
+//     kind: IpAddrKind::V4,
+//     address: String::from("127.0.0.1"),
+// };
+
+// let loopback = IpAddr {
+//     kind: IpAddrKind::V6,
+//     address: String::from("::1"),
+// };
+
+// // - 'Advanced' Way - //
+// enum IpAddr {
+//     V4(String),
+//     V6(String),
+// }
+
+// let home = IpAddr::V4(String::from("127.0.0.1"));
+
+// let loopback = IpAddr::V6(String::from("::1"));
+
+// // - Different Data Types - //
+// enum IpAddr {
+//     V4(u8, u8, u8, u8),
+//     V6(String),
+// }
+
+// let home = IpAddr::V4(127, 0, 0, 1);
+
+// let loopback = IpAddr::V6(String::from("::1"));
+
+// // - Option<T> Enum, Some, None - //
+// let some_number = Some(5);
+// let some_char = Some('e');
+
+// let absent_number: Option<i32> = None;
+
+// // - Match Control Flow Operator - //
+// enum Coin {
+//     Penny,
+//     Nickel,
+//     Dime,
+//     Quarter,
+// }
+
+// fn value_in_cents(coin: Coin) -> u8 {
+//     match coin {
+//         Coin::Penny => 1,
+//         Coin::Nickel => 5,
+//         Coin::Dime => 10,
+//         Coin::Quarter => 25,
+//     }
+// }
+
+#[derive(Debug)] // so we can inspect the state in a minute
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
 }
 
-struct IpAddr {
-    kind: IpAddrKind,
-    address: String,
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
 }
 
-let home = IpAddr {
-    kind: IpAddrKind::V4,
-    address: String::from("127.0.0.1"),
-};
-
-let loopback = IpAddr {
-    kind: IpAddrKind::V6,
-    address: String::from("::1"),
-};
-
-// - 'Advanced' Way - //
-enum IpAddr {
-    V4(String),
-    V6(String),
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {state:?}!");
+            25
+        }
+    }
 }
 
-let home = IpAddr::V4(String::from("127.0.0.1"));
+fn main() {
+    let value: u8 = value_in_cents(Coin::Quarter(UsState::Alaska));
 
-let loopback = IpAddr::V6(String::from("::1"));
-
-// - Different Data Types - //
-enum IpAddr {
-    V4(u8, u8, u8, u8),
-    V6(String),
+    println!("Value: {value}");
 }
-
-let home = IpAddr::V4(127, 0, 0, 1);
-
-let loopback = IpAddr::V6(String::from("::1"));
-
-// - Option<T> Enum, Some, None - //
-let some_number = Some(5);
-let some_char = Some('e');
-
-let absent_number: Option<i32> = None;
