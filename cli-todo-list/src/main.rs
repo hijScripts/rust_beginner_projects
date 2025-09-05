54bf43ecbfaa1aeda71ec92d3a16d3615d9ac693
Harrison J <harrisonjenkins1902@gmail.com>
2025-09-05 13:41:17 +0930

    practiced using vector to store tasks

--- cli-todo-list/src/main.rs
+++ cli-todo-list/src/main.rs
@@ -5,17 +5,36 @@ use crate::manage_tasks::tasks;
 use crate::manage_cli::cli;
 
 fn main() {
-    let mut task1 = tasks::Task::new(String::from("Finish Rust project"));
-    println!("Task: {}, Completed: {}", task1.name, task1.completed);
-    
-    task1.mark_complete();
-    println!("Task: {}, Completed: {}", task1.name, task1.completed);
+    let mut vec_of_tasks: Vec<tasks::Task> = Vec::new();
+    let mut user_input: String = String::from("");
 
-    println!("Please enter a new task:");
-    let new_task_name = cli::get_input();
-    let mut task2 = tasks::Task::new(new_task_name);
-    println!("New Task: {}, Completed: {}", task2.name, task2.completed);
+    loop {
+        println!("Enter a task name: ");
+        user_input = cli::get_input();
 
-    task2.mark_complete();
-    println!("Task: {}, Completed: {}", task2.name, task2.completed);
+        let user_input = user_input.trim().to_lowercase();
+
+        if user_input == "exit" {
+            println!("Exiting...");
+            break;
+        }
+
+        let new_task = tasks::Task::new(user_input);
+        vec_of_tasks.push(new_task);
+    }
+
+    // before completion
+    for task in &vec_of_tasks {
+        println!("{}: {}", task.name, task.completed);
+    }
+
+    for task in &mut vec_of_tasks {
+        task.mark_complete();
+    }
+
+    // after completion
+    println!();
+    for task in &vec_of_tasks {
+        println!("{}: {}", task.name, task.completed);
+    }
 }
